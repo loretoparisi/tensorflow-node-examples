8df67368e92db16ce2cc8ccbdd904e45ad021933
Loreto Parisi <loretoparisi@gmail.com>
2018-04-24 18:58:55 +0200

    Initial commit

--- getting_started/README.md
+++ getting_started/README.md
@@ -1,10 +1,6 @@
-# tensorflow-node-examples
-Tensorflow Node.js Examples
-
-## getting started
+#tensorflow-node examples
 To run the example first install dependencies and link locally the `tfjs-node` module
 ```
-cd getting_started
 yarn
 yarn link-local tfjs-node
 node index.js
---
+++ getting_started/index.js
@@ -0,0 +1,34 @@
+
+
+var tf = require('@tensorflow/tfjs');
+var bindTensorFlowBackend =  require('tfjs-node').bindTensorFlowBackend;
+bindTensorFlowBackend();
+
+// Tiny TFJS train / predict example.
+function myFirstTfjs() {
+    // Create a simple model.
+    const model = tf.sequential();
+    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
+  
+    // Prepare the model for training: Specify the loss and the optimizer.
+    model.compile({
+      loss: 'meanSquaredError',
+      optimizer: 'sgd'
+    });
+  
+    // Generate some synthetic data for training. (y = 2x - 1)
+    const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+    const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+  
+    // Train the model using the data.
+    model.fit(xs, ys, {epochs: 500});
+  
+    // Use the model to do inference on a data point the model hasn't seen.
+    // Should print approximately 39.
+    var predict=
+        model.predict(tf.tensor2d([20], [1, 1]));
+
+    console.log( predict );
+  }
+  
+  myFirstTfjs();
