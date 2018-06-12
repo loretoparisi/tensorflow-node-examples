484c48f2764a9884c78de5118cb659a3cd7ecb16
Loreto Parisi <loretoparisi@gmail.com>
2018-06-12 17:08:52 +0200

    Fixes

--- getting_started/index.js
+++ getting_started/index.js
@@ -1,34 +1,30 @@
-
-
 require('@tensorflow/tfjs-node');
 var tf = require('@tensorflow/tfjs');
 tf.setBackend('tensorflow');
-
-// Tiny TFJS train / predict example.
 function myFirstTfjs() {
-    // Create a simple model.
-    const model = tf.sequential();
-    model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-  
-    // Prepare the model for training: Specify the loss and the optimizer.
-    model.compile({
-      loss: 'meanSquaredError',
-      optimizer: 'sgd'
-    });
-  
-    // Generate some synthetic data for training. (y = 2x - 1)
-    const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-    const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
-  
-    // Train the model using the data.
-    model.fit(xs, ys, {epochs: 500});
-  
-    // Use the model to do inference on a data point the model hasn't seen.
-    // Should print approximately 39.
-    var predict=
-        model.predict(tf.tensor2d([20], [1, 1]));
+  const model = tf.sequential();
+  model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
+  model.compile({
+    loss: 'meanSquaredError',
+    optimizer: 'sgd'
+  });
+  // Generate some synthetic data for training. (y = 2x - 1)                                                                                                            
+  const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+  const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+  // Train the model using the data.                                                                                                                                    
+  model.fit(xs, ys, { epochs: 500 })
+      .then(res => {
+        p10 = model.predict(tf.tensor2d([5.5], [1, 1]))
+        pExp10 = tf.exp(p10);
+        console.log("2*5.5 - 1 SHOULD BE CLOSE TO 10");
+        console.log(p10.dataSync());
+        console.log("exp(10) SHOULD BE CLOSE TO 22026");
+        console.log(pExp10.dataSync());
+      })
+      .catch(error => {
+        console.error(error);
+      })
+}
 
-    console.log( "PREDICT\n", predict );
-  }
-  
-  myFirstTfjs();
+console.log("hello world");
+myFirstTfjs();
